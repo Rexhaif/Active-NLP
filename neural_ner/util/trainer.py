d4dc48dd723eb38e9db99574a7f1222427968b7e
s.ustyantsev <s.ustyantsev@skoltech.ru>
2020-10-28 14:48:09 +0300

    fix add logging

--- neural_ner/util/trainer.py
+++ neural_ner/util/trainer.py
@@ -92,7 +92,7 @@ class Trainer(object):
                 
                 if count % plot_every == 0:
                     loss /= plot_every
-                    log.info(word_count, ': ', loss)
+                    log.info(f'{word_count}:  {loss}')
                     if losses == []:
                         losses.append(loss)
                     losses.append(loss)
